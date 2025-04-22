425dd7737e92ac0d0c79e4f64584223b4cb0ac8c
ethan davies <edavies9000@gmail.com>
2025-04-22 19:21:25 +0100

    Django setup/settings

--- dom6modgen/dom6modgen/settings.py
+++ dom6modgen/dom6modgen/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nations.apps.NationsConfig',
+    'units.apps.UnitsConfig',
+    'spells.apps.SpellsConfig',
+    'items.apps.ItemsConfig',
 ]
 
 MIDDLEWARE = [
