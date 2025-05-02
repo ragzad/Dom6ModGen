31cb31628cb5d87790c1ea6dba561d17b076931a
ethan davies <edavies9000@gmail.com>
2025-05-02 18:16:37 +0100

    Update settings.py

--- dom6modgen/settings.py
+++ dom6modgen/settings.py
@@ -47,6 +47,12 @@ TEMPLATES = [
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request', 
+                'django.contrib.auth.context_processors.auth', 
+                'django.contrib.messages.context_processors.messages', 
+            ],
         },
     },
 ]
