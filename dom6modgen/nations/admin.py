b13bd1cca851934a0427959cc01a76c482a7430b
ethan davies <edavies9000@gmail.com>
2025-05-10 11:17:47 +0100

    Update admin.py

--- dom6modgen/nations/admin.py
+++ dom6modgen/nations/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Nation 
+from .models import Nation, ModGenerationJob
 
 admin.site.register(Nation)
 admin.site.register(ModGenerationJob) # Register the new model
