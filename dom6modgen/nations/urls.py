a68f85a661c6209b7358f41d962735c930f3f355
ethan davies <edavies9000@gmail.com>
2025-05-22 12:25:39 +0100

    Update urls.py

--- dom6modgen/nations/urls.py
+++ dom6modgen/nations/urls.py
@@ -1,13 +1,17 @@
-from django.urls import path
-from . import views # Import views from the current directory (nations app)
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 
-app_name = 'nations' 
+# Import settings and static for development serving
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
-    path('', views.nation_list, name='nation_list'),
-    path('<int:pk>/', views.nation_detail, name='nation_detail'),
-    path('new/', views.nation_create, name='nation_create'),
-    path('<int:pk>/edit/', views.nation_update, name='nation_update'),
-    path('<int:pk>/delete/', views.nation_delete, name='nation_delete'),
-    path('<int:pk>/generate_dm/', views.nation_generate_dm, name='nation_generate_dm'),
+    # Using a direct path '/nations/' for the redirect URL.
+    path('', RedirectView.as_view(url='/nations/'), name='home'),
+
+    # Standard path for the Django admin interface.
+    path('admin/', admin.site.urls),
+    # Including URLs from the 'nations' app under the '/nations/' prefix.
+    path('nations/', include('nations.urls')),
 ]
